0004feeffb1f4ed267fb0962fc3d0d2935cd74f9
mikiplaygames <95536396+mikiplaygames@users.noreply.github.com>
2023-12-01 14:18:07 +0100

    more rotation

--- Assets/Scripts/Orbit.cs
+++ Assets/Scripts/Orbit.cs
@@ -2,11 +2,29 @@ using UnityEngine;
 
 public class Orbit : MonoBehaviour
 {
-    public Transform target;
     public float speed = 10f;
-
+    public bool enableSetter = false;
+    public int i = 0;
+    private Vector3 upper;
+    private void OnGUI()
+    {
+        if (enableSetter)
+            if (GUI.Button(new Rect(300, 10, 150, 100), "sample next"))
+                i = (i + 1) % 3;
+    }
     void Update()
     {
-        transform.RotateAround(target.position, Vector3.up, speed * Time.deltaTime);
+        switch (i)
+        {
+            case 0:
+                transform.RotateAround(Vector3.zero, transform.up, speed * Time.deltaTime);
+                break;
+            case 1:
+                transform.RotateAround(Vector3.zero, transform.right, speed * Time.deltaTime);
+                break;
+            case 2:
+                transform.RotateAround(Vector3.zero, transform.forward, speed * Time.deltaTime);
+                break;
+        }
     }
 }
