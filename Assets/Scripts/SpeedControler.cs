1cfff4b471429643f14ec8b1a3d4074ec6e32800
mikiplaygames <gajewskimikolaj00@gmail.com>
2023-12-01 17:16:12 +0100

    speedometer

--- Assets/Scripts/SpeedControler.cs
+++ Assets/Scripts/SpeedControler.cs
@@ -1,11 +1,20 @@
+using System;
 using UnityEngine;
 
 public class SpeedControler : MonoBehaviour
 {
+    [SerializeField] private RectTransform spedometer;
     public static int Speed = 1;
-
+    private void Awake()
+    {
+        RefreshSpedometer();
+    }
     public void SetSpeed(float i)
     {
         Speed = (int)i;
     }
+    public void RefreshSpedometer()
+    {
+        spedometer.rotation = Quaternion.Euler(0, 0, Mathf.Lerp(170,-60,Speed/1000f));
+    }    //170 - -60
 }
