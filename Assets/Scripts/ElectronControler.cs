82e7bd5447e47cf4d584f2c49d1fd4536a4178a7
mikiplaygames <gajewskimikolaj00@gmail.com>
2023-12-05 10:13:50 +0100

    Work all

--- Assets/Scripts/ElectronControler.cs
+++ Assets/Scripts/ElectronControler.cs
@@ -5,34 +5,23 @@ using Random = UnityEngine.Random;
 public class ElectronControler : MonoBehaviour
 {
     [SerializeField] private bool increment = false;
-    private void OnEnable()
-    {
-        if (increment)
-            StartCoroutine(Lower());
-    }
-    private IEnumerator Lower()
-    {
-        while (enabled)
-        {
-            yield return new WaitForSeconds(Random.Range(0.01f, 5));
-            if (transform.localPosition.x > 3)
-                transform.localPosition -= Vector3.right;
-        }
-    }
     private void OnTriggerEnter(Collider other)
     {
-        if (increment && transform.localPosition.x < 5)
+        if (increment && transform.localPosition.x < 6)
         {
             transform.localPosition += Vector3.right;
             StartCoroutine(ReleaseAfterTime(other.transform));
         }
+        else
+            PhotonControler.Instance.photons.Release(other.transform);
     }
     private IEnumerator ReleaseAfterTime(Transform other)
     {
         var rb = other.GetComponent<Rigidbody>();
         rb.velocity = Vector3.zero;
         other.gameObject.SetActive(false);
-        yield return new WaitForSeconds(Random.Range(0.1f, 3f));
+        yield return new WaitForSeconds(Random.Range(0.01f, 5f));
+        transform.localPosition -= Vector3.right;
         other.gameObject.SetActive(true);
         rb.AddForce(new Vector3(Random.Range(-1f,1f),Random.Range(-1f,1f),0) * 1000);
     }
